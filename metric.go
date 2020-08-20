06040add4573084e22fa4baae605b1529c370607
116356754@qq.com <116356754@qq.com>
2020-08-20 17:20:39 +0800

    删除不必要的结构体

--- metric.go
+++ metric.go
@@ -6,24 +6,29 @@
  */
 package IOModel
 
+const (
+	QualityBad  uint8 = 0x00
+	QualityGood uint8 = 0x01
+)
+
 //采集记录的数据结构
 type GoField struct {
-	Id        string  `json:"id"`
-	Status    uint8  `json:"status"`
-	Val       IOValue `json:"val"`
-	Timestamp int64   `json:"timestamp"`
+	Id        string  `json:"id"`        //每个点的唯一id
+	Status    uint8   `json:"status"`    //每个点的质量戳
+	Val       IOValue `json:"val"`       //每个点的数据类型可能不一样
+	Timestamp int64   `json:"timestamp"` //每个点得到的时间戳
 }
 
 //采集端的数据模型，由设备和下属点构成
 type GoMetric struct {
 	DeviceId  string             //设备的id
-	Status    bool               //表示设备的状态
+	Status    uint8              //表示设备的状态
 	Fields    map[string]GoField //设备下面的记录集
 	Timestamp int64              //表示最后一次更新的时间
 }
 
 //反写操作的数据结构
 type GoWriteRecord struct {
-	TagId    string `json:"id"`
+	TagId    string  `json:"id"`
 	TagValue IOValue `json:"val"`
 }
